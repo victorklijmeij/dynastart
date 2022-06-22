8975599d5589e4e7b2acee798f2991834b5ead91
Victor Klijmeij <victor@klijmeij.com>
2022-06-22 16:40:13 +0200

    Basic version with search and tag filters

--- dynastart.js
+++ dynastart.js
@@ -4,31 +4,28 @@ document.addEventListener("DOMContentLoaded", function(){
     nativeObject = YAML.parse($("#source").val());
     console.log(nativeObject);
     dyna_searchvalue="";
-    dynastart_search_and_update();
+    contenttag=window.location.hash.substr(1);
+    dynastart_search_and_update(contenttag);
 });
 
 
-function dynastart_search_and_update() { 
-    contenttag=window.location.hash.substr(1);
-    console.log("function call search and update");
-    // console.log(nativeObject);
-    // console.log("func "+$("#searchTheKey").val());
-
+function dynastart_search_and_update(searchtag=contenttag) { 
+    contenttag=searchtag;
     console.log("search for "+dyna_searchvalue+" with tag: "+contenttag);
     for (idx = 0; idx < nativeObject.length; idx++) {
         if (nativeObject[idx].name.toLowerCase().search(dyna_searchvalue) > -1) {
-            if ($.inArray(contenttag, nativeObject[idx].tags) != -1 || contenttag =='' ) {
-                console.log("tag "+contenttag+" in "+nativeObject[idx].tags);
+            if ($.inArray(searchtag, nativeObject[idx].tags) != -1 || searchtag =='' ) {
+                console.log("tag "+searchtag+" in "+nativeObject[idx].tags);
                 if($("#linkid" + idx).length == 0) {
                 console.log("<a href=\""+nativeObject[idx].url+"\">"+nativeObject[idx].name+"\"</a>");
                 $("#searchresults").append("<li  id=linkid"+idx+""+"><a href=\""+nativeObject[idx].url+"\">"+nativeObject[idx].name+"</a></li>");
                 } 
             } else {
-                console.log("tag "+contenttag+" not in "+nativeObject[idx].tags);
+                // remove items with missing tag
                 $("#searchresults #linkid"+idx).remove();
             }
         } else {
-            // console.log("remove #searchresults #linkid"+idx);
+            // Remove items with missing search word
             $("#searchresults #linkid"+idx).remove();
         }
     }
